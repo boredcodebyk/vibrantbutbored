eb4b2e3514d2ae9b7f9ed8eaeea8025100c13ea3
boredcodebyk <25322315+boredcodebyk@users.noreply.github.com>
2024-07-09 13:56:09 +0530

    wip Button and Link

---
+++ src/lib/Button/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+    $: props = {
+        class: [
+            "vbb-btn"
+        ]
+    }
+</script>
+
+<button
+    on:click
+    on:focus
+    on:blur
+    on:mouseover
+    on:mouseenter
+    on:mouseleave
+    {...props}
+>
+    <slot/>
+</button>
---
+++ src/lib/Link/Link.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let href;
+
+    $: props = {
+        class: [
+            "vbb-link"
+        ]
+    }
+</script>
+
+<a {...props} href="{href}"><slot/></a>
